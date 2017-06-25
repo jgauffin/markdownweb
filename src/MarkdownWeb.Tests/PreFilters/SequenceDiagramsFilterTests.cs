7bdfe0096ee7a622597c7a52abaec13da480c56b
gauffininteractive <info@onetrueerror.com>
2017-06-25 22:23:10 +0200

    * Updated nuget packages
    * Switched to highlightjs
    * Added support for sequence diagrams
    * Adjusted UI in the example project

---
+++ src/MarkdownWeb.Tests/PreFilters/SequenceDiagramsFilterTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MarkdownWeb.PreFilters;
+using Xunit;
+
+namespace MarkdownWeb.Tests.PreFilters
+{
+    public class SequenceDiagramsFilterTests
+    {
+        [Fact]
+        public void should_Be_able_to_parse_a_block_that_ends_the_Document()
+        {
+            var markdown = @"# Hello world
+
+This is a test
+
+```sequence
+A->B: Hejsan
+B->A: Oooopsie
+```";
+            var context = new PreFilterContext(new FakeService()) {TextToParse = markdown};
+
+            var sut = new SequenceDiagramsFilter();
+            var result = sut.Parse(context);
+
+            var id = sut.Ids.First();
+            result.Should().Be($@"# Hello world
+
+This is a test
+
+<div class=""sequencediagram"" id=""{id}""></div>
+<script> 
+    var d = Diagram.parse('A->B: Hejsan
+B->A: Oooopsie');
+    var options = {{ theme: 'Simple'}};
+    d.drawSVG('{id}', options);
+</script>");
+        }
+
+        [Fact]
+        public void should_Be_able_to_parse_multiple_blocks_in_a_document()
+        {
+            var markdown = @"# Hello world
+
+This is a test
+
+```sequence
+A->B: Hejsan
+B->A: Oooopsie
+```
+
+Here comes the next one:
+
+```sequence
+Client->Server: Authenticate
+Server->Client: Challenge(nonce)
+Client->Server: Handshake
+```";
+            var context = new PreFilterContext(new FakeService()) { TextToParse = markdown };
+
+            var sut = new SequenceDiagramsFilter();
+            var result = sut.Parse(context);
+
+            result.Should().Be($@"# Hello world
+
+This is a test
+
+<div class=""sequencediagram"" id=""{sut.Ids[0]}""></div>
+<script> 
+    var d = Diagram.parse('A->B: Hejsan
+B->A: Oooopsie');
+    var options = {{ theme: 'Simple'}};
+    d.drawSVG('{sut.Ids[0]}', options);
+</script>
+
+Here comes the next one:
+
+<div class=""sequencediagram"" id=""{sut.Ids[1]}""></div>
+<script> 
+    var d = Diagram.parse('Client->Server: Authenticate
+Server->Client: Challenge(nonce)
+Client->Server: Handshake');
+    var options = {{ theme: 'Simple'}};
+    d.drawSVG('{sut.Ids[1]}', options);
+</script>");
+        }
+
+        [Fact]
+        public void should_Be_able_to_parse_a_block_in_the_middle_of_the_document()
+        {
+            var markdown = @"# Hello world
+
+This is a test
+
+```sequence
+A->B: Hejsan
+B->A: Oooopsie
+```
+
+Some other paragraph.";
+            var context = new PreFilterContext(new FakeService()) { TextToParse = markdown };
+
+            var sut = new SequenceDiagramsFilter();
+            var result = sut.Parse(context);
+
+            var id = sut.Ids.First();
+            result.Should().Be($@"# Hello world
+
+This is a test
+
+<div class=""sequencediagram"" id=""{id}""></div>
+<script> 
+    var d = Diagram.parse('A->B: Hejsan
+B->A: Oooopsie');
+    var options = {{ theme: 'Simple'}};
+    d.drawSVG('{id}', options);
+</script>
+
+Some other paragraph.");
+        }
+
+        [Fact]
+        public void should_Be_able_to_parse_a_block_in_the_start_of_the_document()
+        {
+            var markdown = @"```sequence
+A->B: Hejsan
+B->A: Oooopsie
+```
+
+Some other paragraph.";
+            var context = new PreFilterContext(new FakeService()) { TextToParse = markdown };
+
+            var sut = new SequenceDiagramsFilter();
+            var result = sut.Parse(context);
+
+            var id = sut.Ids.First();
+            var expected = $@"<div class=""sequencediagram"" id=""{id}""></div>
+<script> 
+    var d = Diagram.parse('A->B: Hejsan
+B->A: Oooopsie');
+    var options = {{ theme: 'Simple'}};
+    d.drawSVG('{id}', options);
+</script>
+
+Some other paragraph.";
+            result.Should().Be(expected);
+        }
+    }
+}
--- src/MarkdownWeb/Properties/AssemblyInfo.cs
+++ src/MarkdownWeb/Properties/AssemblyInfo.cs
@@ -13,6 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015 Jonas Gauffin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("MarkdownWeb.Tests")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
